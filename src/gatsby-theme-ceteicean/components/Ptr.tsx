918aee48f0eed4afa56540d1b70258d76316f49c
raffazizzi <raffaeleviglianti@gmail.com>
2022-11-18 14:06:35 -0500

    simpler footnotes

--- src/gatsby-theme-ceteicean/components/Ptr.tsx
+++ src/gatsby-theme-ceteicean/components/Ptr.tsx
@@ -10,28 +10,13 @@ import { styled } from '@mui/material/styles'
 import estyled from '@emotion/styled'
 
 import { Colors, IColors } from '../../displayOptions'
+import { Box } from "@mui/system"
 
 
 type TEIProps = {
   teiNode: Node,
   availableRoutes?: string[]
 }
-
-const Spacer = estyled.div((props: {length: string}) => ({
-  width: props.length
-}))
-
-const Footnote = styled(Badge)<BadgeProps & {length: string, typeColor: string}>(({ theme, length, typeColor }) => ({
-  '& .MuiBadge-badge': {
-    top: '-.5em',
-    right: length,
-    backgroundColor: typeColor,
-    border: `2px solid ${theme.palette.background.paper}`,
-    padding: '0 4px',
-    cursor: 'pointer',
-    textIndent: 0,
-  },
-}));
 
 const Ptr: TBehavior = (props: TEIProps) => {
 
@@ -55,34 +40,16 @@ const Ptr: TBehavior = (props: TEIProps) => {
       n: parseInt(n)
     }
 
-    let spacerlength: string
-    switch (n.length) {
-      case 1:
-        spacerlength = `1.5rem`
-      case 3:
-        spacerlength = `2rem`
-      default:
-        spacerlength = `${n.length}rem`
-    }
-
-    let fnlength: string
-    switch (n.length) {
-      case 1:
-        fnlength = `.75rem`
-      case 3:
-        fnlength = `.75rem`
-      default:
-        fnlength = `${n.length/2}rem`
-    }
-
     return (
       <Behavior node={props.teiNode} key={n}>
-        <Footnote length={fnlength} badgeContent={n} color="primary" max={999} typeColor={typeColor} onClick={() => {
+        <Box onClick={() => {
           setEntity(null)
           setNote(noteData)
-        }} sx={{wordBreak: "normal"}}>
-          <Spacer length={spacerlength}/>
-        </Footnote>
+        }} component="sup" sx={{
+          color: typeColor,
+          fontSize: "80%",
+          cursor: "pointer"
+        }}>[{n}]</Box>
       </Behavior>
     )
   }
