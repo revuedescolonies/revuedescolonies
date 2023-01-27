64de56db168474863a29a1e21d1da4875f23956e
raffazizzi <raffaeleviglianti@gmail.com>
2023-01-27 21:46:59 +0000

    Deploying to gh-pages from @ revuedescolonies/revuedescolonies@ea2ccdd4118bb38e8f34ca59a4b7f087fa076068 🚀

---
+++ component---src-pages-404-tsx-15a5777d3fb16f07e7c9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkse_microedition_template=self.webpackChunkse_microedition_template||[]).push([[218],{2224:function(e,t,n){n.r(t);var i=n(7453),l=n(3765),o=n(3431);t.default=function(){return(0,o.jsx)(i.Z,{location:"404"},(0,o.jsx)(l.Z,{title:"404: Not found",lang:"en"}),(0,o.jsx)("h1",null,"PAGE NOT FOUND"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-15a5777d3fb16f07e7c9.js.map
