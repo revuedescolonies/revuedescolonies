38482f61daf80ae3b58e4444e4c14b66700f91da
raffazizzi <raffaeleviglianti@gmail.com>
2023-10-27 10:33:00 -0400

    made responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -17,8 +17,11 @@ const styles = {
     borderTop: "2px solid #dadada",
     fontSize: "1rem"
   },
-  logo: {
-    textAlign: "right",
+  logos: {
+    display: "flex",
+    justifyContent: "center",
+    gap: "2em",
+    flexFlow: "wrap"
   }
 }
 
@@ -28,65 +31,60 @@ interface Props {
 
 const Footer = ({repository} : Props) => (
   <Box component="footer" sx={styles.footer}>
-    <Container maxWidth="lg" sx={{display: "flex", justifyContent: "center"}}>
-      <Grid container xs={10} >
-      
-        <Grid item xs={3} sx={{display: "flex", flexDirection: "column"}}>
-          <a
-            rel="license"
-            href="http://creativecommons.org/licenses/by-nc-sa/3.0/"
-          >
-            <img
-              alt="Creative Commons License"
-              src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png"
-            />
-          </a>        
-          <a href="https://www.bnf.fr/fr/gallica-la-bibliotheque-numerique-de-la-bnf-et-de-ses-partenaires" style={{ marginRight: "2.5rem" }}>
-            <img
-              width={150}
-              src={`${Gallica}`}
-              alt="Logo of Gallica, la Bibliothèque numérique de la BnF"
-            />
-          </a> 
-        </Grid> 
-        <Grid item xs={9} sx={{ display: "flex", justifyContent: "flex-end" }}>
-            <a href="https://umd.edu" style={{ marginRight: "2.5rem" }}>
+    <Container maxWidth="md" sx={styles.logos}>
+          <Box sx={{display: "flex", flexDirection: "column"}}>
+            <a
+              rel="license"
+              href="http://creativecommons.org/licenses/by-nc-sa/3.0/"
+            >
+              <img
+                alt="Creative Commons License"
+                src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png"
+              />
+            </a>        
+            <a href="https://www.bnf.fr/fr/gallica-la-bibliotheque-numerique-de-la-bnf-et-de-ses-partenaires">
+              <img
+                style={{maxWidth: "125px"}}
+                src={`${Gallica}`}
+                alt="Logo of Gallica, la Bibliothèque numérique de la BnF"
+              />
+            </a> 
+          </Box> 
+          <a href="https://umd.edu">
             <img
               width={65}
               src={`${umd}`}
               alt="Logo of the University of Maryland"
             />
           </a>
-          <a href="https://mith.umd.edu/" style={{ marginRight: "2.5rem" }}> 
+          <a href="https://mith.umd.edu/"> 
             <img
               width={65}
               src={`${MITH}`}
               alt="Logo of the Maryland Institute for Technology in the Humanities"
             />
           </a>
-            <a href="https://bibsocamer.org/" style={{ marginRight: "2.5rem" }}>
+            <a href="https://bibsocamer.org/">
             <img
               width={65}
               src={`${BSA}`}
               alt="Logo of the Bibliographical Society of America"
             />
             </a>
-            <a href="https://memoire-esclavage.org/fme" style={{ marginRight: "2.5rem" }}>
+            <a href="https://memoire-esclavage.org/fme">
             <img
               width={65}
               src={`${fme}`}
               alt="Logo of the FME"
             />
             </a>
-            <a href="https://www.nypl.org/locations/schomburg" style={{ marginRight: "2.5rem" }}>
+            <a href="https://www.nypl.org/locations/schomburg">
               <img
                 width={360}
                 src={`${Schomburg}`}
                 alt="Logo of the Schomburg Center"
               />
           </a>  
-        </Grid>
-      </Grid>
     </Container>
   </Box>
 )
