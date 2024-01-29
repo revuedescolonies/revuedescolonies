b4344bb795b222a8185ba881314302c691dec801
raffazizzi <raffaeleviglianti@gmail.com>
2024-01-29 13:11:26 -0500

    new logos

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,6 +9,8 @@ import Schomburg from "../images/Schomburg.jpg"
 import MITH from "../images/MITHgrayscale.png"
 import BSA from "../images/BSA.png"
 import Gallica from "../images/Gallica.png"
+import nhprc from "../images/nhprc-logo.png"
+import acls from "../images/acls.png"
 
 const styles = {
   footer: {
@@ -21,7 +23,11 @@ const styles = {
     display: "flex",
     justifyContent: "center",
     gap: "2em",
-    flexFlow: "wrap"
+    flexFlow: "wrap",
+    "& a": {
+      display: "flex",
+      alignSelf: "center"
+    }
   }
 }
 
@@ -36,13 +42,15 @@ const Footer = ({repository} : Props) => (
             <a
               rel="license"
               href="https://creativecommons.org/licenses/by-nc-sa/4.0"
+              style={{alignSelf: "unset", display: "inline"}}
             >
               <img
                 alt="Creative Commons License"
                 src="http://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"
               />
             </a>        
-            <a href="https://www.bnf.fr/fr/gallica-la-bibliotheque-numerique-de-la-bnf-et-de-ses-partenaires">
+            <a href="https://www.bnf.fr/fr/gallica-la-bibliotheque-numerique-de-la-bnf-et-de-ses-partenaires"
+              style={{alignSelf: "unset", display: "inline"}}>
               <img
                 style={{maxWidth: "125px"}}
                 src={`${Gallica}`}
@@ -76,6 +84,20 @@ const Footer = ({repository} : Props) => (
               width={65}
               src={`${fme}`}
               alt="Logo of the FME"
+            />
+            </a>
+            <a href="https://www.archives.gov/nhprc">
+            <img
+              width={65}
+              src={`${nhprc}`}
+              alt="Logo of the NHPRC"
+            />
+            </a>
+            <a href="https://acls.org">
+            <img
+              width={150}
+              src={`${acls}`}
+              alt="Logo of the ACLS"
             />
             </a>
             <a href="https://www.nypl.org/locations/schomburg">
